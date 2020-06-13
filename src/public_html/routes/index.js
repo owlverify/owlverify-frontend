0cb74c38ee9e172cb68156204ad6141d102c31a6
Nitin Goyal <nitingoyal.dev@gmail.com>
2020-06-13 18:13:26 +0530

    Bug fix

--- src/public_html/routes/index.js
+++ src/public_html/routes/index.js
@@ -18,7 +18,7 @@ var storage = multer.diskStorage({
     })
   },
   filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now())
+    cb(null, file.originalname + '-' + Date.now() + '.csv')
   }
 })
 var upload = multer({ storage: storage })
