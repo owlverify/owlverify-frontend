358c31b875a448b995c1c31a64dae6c60f5f8c7e
Nitin Goyal <nitingoyal.dev@gmail.com>
2020-06-13 12:12:13 +0530

    bug fix

--- src/api/models/user.model.js
+++ src/api/models/user.model.js
@@ -112,28 +112,24 @@ userSchema.statics = {
       if (user)
         return fn('This email is already in use.')
 
-      let u = this
-
       let salt = crypto.randomBytes(128).toString('base64')
       console.log(salt)
-      crypto.pbkdf2(password, salt, 5000, 32, 'sha512', (err, derivedKey) => {
-        if (err)
-          return fn('There has been an internal error. Please try again later.')
+      let derivedKey = crypto.pbkdf2Sync(password, salt, 5000, 32, 'sha512')
 
-        console.log(salt)
-        u.create({
-          email: email,
-          password: new Buffer(derivedKey).toString('base64'),
-          salt: salt,
-          //reg_date: new Date()
-        }, (err, result) => {
-          return fn(null, {
-            id: result.ops[0]._id,
-            email: result.ops[0].email,
-          })
-        })
+      console.log('derivedKey', derivedKey)
+      let result = await this.create({
+        email: email,
+        password: new Buffer(derivedKey).toString('base64'),
+        salt: salt,
+        //reg_date: new Date()
+      }).exec()
+
+      console.log(result)
+
+      return fn(null, {
+        id: result.ops[0]._id,
+        email: result.ops[0].email,
       })
-
     } catch (e) {
       console.log(e)
       return fn('There has been an internal error. Please try again later.')
