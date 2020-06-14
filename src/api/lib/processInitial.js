b560c04f5205ec9ac1d5b1eec181409fb59e90ed
Nitin Goyal <nitingoyal.dev@gmail.com>
2020-06-14 16:31:12 +0530

    Bug fix

--- src/api/lib/processInitial.js
+++ src/api/lib/processInitial.js
@@ -1,4 +1,57 @@
+const async = require('async')
 const csv = require('csvtojson')
+
+function queue (worker, work, concurrency) {
+  console.log('started, with concurrency=' + concurrency)
+  return new Promise(function (resolve, reject) {
+    if (work.length === 0) {
+      resolve()
+    } else {
+      var firstError
+
+      var q = async.queue(worker, concurrency)
+      q.drain(function () {
+        resolve()
+      })
+
+      q.error(function (error) {
+        if (firstError === undefined) {
+          // only reject with the first error;
+          firstError = error
+        }
+
+        // don't start any new work
+        q.kill()
+
+        // but wait untill all pending work completes before reporting
+        q.drain(function () {
+          reject(firstError)
+        })
+      })
+      q.push(work)
+    }
+  })
+}
+
+const worker = async.asyncify(function (work) {
+  return new Promise((resolve, reject) => {
+    (async () => {
+      console.log(work)
+
+      /*
+      csv()
+    .fromFile(file.path)
+    .then(async (jsonArr) => {
+      console.log(jsonArr)
+    });
+       */
+
+      resolve()
+    })()
+  })
+})
+
+
 
 module.exports = async (file) => {
   csv()
